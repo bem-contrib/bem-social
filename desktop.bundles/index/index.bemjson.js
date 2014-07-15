22dc14ea00aaf61c7517b15750de08b494008bdc
voischev <voischev.ivan@ya.ru>
2014-07-15 16:54:47 +0400

    add share

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -4,7 +4,11 @@
     head : [
         { elem : 'meta', attrs : { name : 'description', content : '' } },
         { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
-        { elem : 'css', url : '_index.css' }
+        { elem : 'css', url : '_index.css' },
+        {
+            elem : 'css',
+            content : '.share + .share{margin-left:10px}'
+        }
     ],
     scripts : [{ elem : 'js', url : '_index.js' }],
     content : [
@@ -18,6 +22,28 @@
                 image : 'https://avatars1.githubusercontent.com/u/1510217'
             },
             text : 'vkontakte'
+        },
+        {
+            block : 'share',
+            mods : { api : 'facebook' },
+            js : {
+                url : 'https://github.com/voischev/bem-social',
+                title : 'Привет мир',
+                description : 'Share Bem-Social',
+                image : 'https://avatars1.githubusercontent.com/u/1510217'
+            },
+            text : 'facebook'
+        },
+        {
+            block : 'share',
+            mods : { api : 'twitter' },
+            js : {
+                url : 'https://github.com/voischev/bem-social',
+                title : 'Привет мир',
+                description : 'Share Bem-Social',
+                image : 'https://avatars1.githubusercontent.com/u/1510217'
+            },
+            text : 'twitter'
         }
     ]
 })
