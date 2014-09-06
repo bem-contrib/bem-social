cb56621f9af1ae8485883c06dfcc9c1b0269f5f1
Travis <travis@travis-ci.org>
2014-09-06 23:04:20 +0400

    code style fix

--- desktop.blocks/yashare/yashare.browser.js
+++ desktop.blocks/yashare/yashare.browser.js
@@ -1,4 +1,7 @@
-modules.define('yashare', ['i-bem__dom', 'loader_type_js'], function(provide, BEMDOM, Loader) {
+modules.define(
+    'yashare',
+    ['i-bem__dom', 'loader_type_js'],
+    function(provide, BEMDOM, Loader) {
     provide(BEMDOM.decl('yashare', {
         onSetMod : {
             js : {
