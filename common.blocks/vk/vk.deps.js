339c6e0f4c5311df2948056495f0cc628cc04084
Alexej Yaroshevich <alex@qfox.ru>
2015-05-30 16:33:07 +0300

    vk: add tmpl-specs

--- common.blocks/vk/vk.deps.js
+++ common.blocks/vk/vk.deps.js
@@ -1,7 +1,12 @@
-({
-    mustDeps : [],
+[{
+    mustDeps : [{ block : 'i-bem', elem : 'dom' }],
     shouldDeps : [
         { block : 'loader', mods : { type : 'js' } },
         { elem : 'config' }
     ]
-})
+}, {
+    tech : 'tmpl-spec.js',
+    mustDeps : [
+        { mods : { widget : 'like' } },
+    ]
+}]
