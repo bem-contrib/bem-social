e6e9b82f518d6bb634754d9515e662a29ddb3481
Alexej Yaroshevich <alex@qfox.ru>
2015-05-30 15:43:59 +0300

    og: add tmpl-specs

---
+++ common.blocks/og/og.tmpl-specs/01-plain.bemjson.js
@@ -0,0 +1,18 @@
+[
+    { block : 'og', title : 'Passing title!' },
+    { block : 'og', description : 'Passing description!' },
+    { block : 'og', type : 'Passing type!' },
+    { block : 'og', keywords : 'Passing keywords!' },
+    { block : 'og', url : 'Passing url!' },
+    { block : 'og', image : 'Passing image!' },
+
+    {
+        block : 'og',
+        title : 'Altogether: title!',
+        description : 'Altogether: description!',
+        type : 'Altogether: type!',
+        keywords : 'Altogether: keywords!',
+        url : 'Altogether: url!',
+        image : 'Altogether: image!'
+    },
+]
