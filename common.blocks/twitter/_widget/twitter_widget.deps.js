870d04d56d709f4388065b3704d55a5ec3df276a
Alexej Yaroshevich <alex@qfox.ru>
2015-05-30 16:25:04 +0300

    twitter: add tmpl-specs

---
+++ common.blocks/twitter/_widget/twitter_widget.deps.js
@@ -0,0 +1,3 @@
+({
+    mustDeps : 'twitter'
+})
---
+++ common.blocks/twitter/twitter.deps.js
@@ -0,0 +1,11 @@
+[{
+    mustDeps : [
+        { block : 'i-bem', elems : ['dom'] }
+    ]
+}, {
+    tech : 'tmpl-spec.js',
+    mustDeps : [
+        { mods : { widget : 'share' } },
+        { mods : { widget : 'tweet' } }
+    ]
+}]
