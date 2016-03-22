b6b2d9388879d1a894a3c6c510f10d133511ad9d
adinvadim <adin.vadim@gmail.com>
2016-03-22 21:59:05 +0300

    Remove old field selector, add data-bare

--- common.blocks/yashare/yashare.bh.js
+++ common.blocks/yashare/yashare.bh.js
@@ -4,16 +4,17 @@ module.exports = function(bh) {
         ctx.js(true);
         ctx.cls('ya-share2');
         ctx.attrs({
-            'data-services' : (json.services || json.quickServices || []).join(','),
+            'data-bare' : json.bare,
             'data-counter' : json.counter,
             'data-copy' : json.copy,
             'data-description' : json.description,
             'data-image' : json.image,
             'data-lang' : json.lang,
             'data-limit' : json.limit,
+            'data-services' : (json.services || []).join(','),
             'data-size' : json.size,
             'data-title' : json.title,
-            'data-url' : json.url
+            'data-url' : json.url,
         });
     });
 
